840c22b2d8e9c9b3a80e08d654e02f8c35c0b68d
Durga Sandeep Vasarla <Vdurgasandeep@gmail.com>
2024-03-04 16:06:56 +0530

    first

--- my-app/src/components/loginpage.jsx
+++ my-app/src/components/loginpage.jsx
@@ -1,44 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 import './loginpage.css';
-import { useFormik } from 'formik';
-import Dataval from './dataval';
+import { useFormik } from "formik";
+import * as yup from 'yup';
+
+
 function Loginpage() {
-    var [arr, setarr] = React.useState([])
+    const [arr, setArr] = useState([]);
+  
 
-    let formik = useFormik({
+    const formik = useFormik({
         initialValues: {
-            username : '', 
-            password: ''
+            username: "",
+            lastname: "",
+            password: ""
         },
-        onSubmit:(values)=> {
-            setarr([...arr, values])
+        validationSchema: yup.object({
+            username: yup.string().required("*").min(2).max(50),
+            lastname: yup.string().required("*").min(2).max(50),
+            password: yup.string().required("*").min(8),
+            email: yup.string().email("InValid Mail").required("*")
+        }),
+        onSubmit: (values) => {
+            console.log(values)
         }
-    })
-  return (
-    <div className='datacontainer'>
-    <form onSubmit={formik.handleSubmit}>
-        <h1>Login page</h1>
-      
-            <input type="text" name='username' placeholder='username' onChange={formik.handleChange}/>
-        
-            <input type="password" name='password' onChange={formik.handleChange} />
-   
-            <button>Submit</button>
-
-    </form>
-
-    <div>
-                
-    <Dataval arr={arr}></Dataval>
-    </div>
-
-    </div>
-    
-    
-
-
-    
-  )
+    });
+    return (
+        <div className="datacontainer">
+            <form onSubmit={formik.handleSubmit} className="formcontainer">
+                <h1 >Registration page</h1>
+                <label className="labeltext">FirstName:-</label> <input type="text" name="username" onChange={formik.handleChange} placeholder="Username"/><br />
+                {formik.errors.username ? <div className="errormsg">{formik.errors.username}</div> : <></>}
+                <label htmlFor="" className="labeltext">LastName:-</label> <input type="text" name="lastname" onChange={formik.handleChange} placeholder="Lastname"/><br />
+                {formik.errors.lastname ? <div className="errormsg">{formik.errors.lastname}</div> : null}
+                <label className="labeltext">Password:-</label> <input type="password" name="password" onChange={formik.handleChange} placeholder="Password"/><br />
+                {formik.errors.password ? <div className="errormsg">{formik.errors.password}</div> : <></>}
+                <label>Gmail:- </label> 
+                {formik.errors.email ? <span  className="errormsg">{formik.errors.email}</span>: null}
+                 <br /><input type="text"  name="email" onChange={formik.handleChange} placeholder="Enter Gmail"/>
+                <button className="btn btn-primary p-3 m-3" type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
 
-export default Loginpage
+export default Loginpage;
